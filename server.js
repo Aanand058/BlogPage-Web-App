0dae518ba813963a4f00e730af51efae6010e01e
Aanand Aman <yamit058@gmail.com>
2023-02-01 13:00:42 -0500

    commit

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
-var HTTP_PORT = process.env.PORT || 8080;
 
+const HTTP_PORT = process.env.PORT || 8080;
+
+const path = require("path");
+
+app.use(express.static('public')); 
+
+const blog= require("./blog-service.js");
 
 
 // call this function after the http server starts listening for requests
@@ -14,10 +20,40 @@ function onHttpStart() {
 }
 
 
-app.use(express.static('public'));
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/about.html"));
-});  
 
 
+//About redirect 
+app.get("/", (req,res) => {
+    res.redirect("/about");
+  });
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+})
+
+
+// ========== Blog Page Route ==========
+app.get("/blog", (req, res) => {
+    res.send("TODO: get all posts who have published==true");
+  })
+
+app.get("/categories", (req,res)=> {
+    res.send("Todo: ");
+})
+
+
+app.get("/categories", (req,res)=>{
+    req.send("todo: ");
+})
+
+
+app.get("/posts", (req,res)=>{res.send("TODO: ")})
+
+
+app.use((req, res) => {
+    res.status(404).send("Sorry, Page Not Found");
+  });
+
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
