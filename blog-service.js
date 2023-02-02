36f67cd0ef85c25bda42d879a4cfd47d0047d757
Aanand Aman <yamit058@gmail.com>
2023-02-01 23:06:15 -0500

    sl

--- blog-service.js
+++ blog-service.js
@@ -2,3 +2,76 @@
 
 
 const fs = require("fs");
+const path = require("path");
+
+//Global Variables 
+let posts = [];
+let categories = [];
+let published = [];
+
+function initialize() {
+
+
+    return new Promise((resolve, reject) => {
+        //reading the posts.json 
+        fs.readFile(path.join(__dirname + "/data/posts.json"), 'utf8', (err, data) => {
+            if (err) { reject("Unable to read file"); }
+
+            posts = JSON.parse(data);
+
+        });
+
+
+        //reading the categories.json
+        fs.readFile(path.join(__dirname + "/data/categories.json"), 'utf8', (err, data) =>{
+            if(err){reject("Unable to read file");}
+
+            categories = JSON.parse(data);
+            resolve();
+
+        });
+    
+    })
+}
+
+
+
+function getAllPosts(){
+    return new Promise((resolve,reject) => {
+        if(posts.length === 0){
+            reject("no results returned");
+        }
+        resolve(posts);
+    });
+}
+
+function getPublishedPosts(){
+    return new Promise((resolve,reject)=>{
+        posts.forEach((post)=>{
+            if(post.published === true){
+                published.push(post);
+            }
+        })
+        if(published.length === 0){
+            reject("no results returned"
+            );
+
+        }
+        resolve(published);
+
+
+    })
+}
+
+
+function getCategories(){
+    return new Promise((resolve,reject)=>{
+        if(categories.length ===0){
+            reject("no results returned");
+        }
+        resolve(categories);
+    })
+}
+
+
+module.exports = { initialize, getPublishedPosts, getAllPosts, getCategories };
--- server.js
+++ server.js
@@ -4,13 +4,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const blog= require("./blog-service.js");
+const blog = require("./blog-service.js");
 
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 
 
@@ -23,37 +23,63 @@ function onHttpStart() {
 
 
 //About redirect 
-app.get("/", (req,res) => {
-    res.redirect("/about");
-  });
+app.get("/", (req, res) => {
+  res.redirect("/about");
+});
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.sendFile(path.join(__dirname, "views", "about.html"));
 })
 
 
-// ========== Blog Page Route ==========
+//  Blog Page 
 app.get("/blog", (req, res) => {
-    res.send("TODO: get all posts who have published==true");
+  blog.getPublishedPosts()
+  .then((data)=>{
+    res.send({data})
   })
-
-app.get("/categories", (req,res)=> {
-    res.send("Todo: ");
+  .catch((err)=>{
+    res.send(err)
+  });
+ 
 })
 
 
-app.get("/categories", (req,res)=>{
-    req.send("todo: ");
+
+//Cateories Page
+app.get("/categories", (req, res) => {
+  blog.getCategories().then((data)=>{
+    res.send({data})
+  })
+  .catch((err)=>{
+    res.send(err)
+  });
 })
 
 
-app.get("/posts", (req,res)=>{res.send("TODO: ")})
+
+//Posts page
+app.get("/posts", (req, res) => { 
+  blog.getAllPosts().then((data)=>{
+    res.send({data})
+  })
+  .catch((err)=>{
+    res.send(err)
+  });
+ })
 
 
 app.use((req, res) => {
-    res.status(404).send("Sorry, Page Not Found");
-  });
+  res.status(404).send("Sorry, Page Not Found");
+});
 
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+
+blog.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart);
+})
+  .catch(() => {
+    console.log("Error Starting the Server. ");
+  });
+
